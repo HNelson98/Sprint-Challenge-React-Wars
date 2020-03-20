ba19582f53337d41c60941872bd08f4cefc554c6
Henry Nelson <Henry.Nelson89@gmail.com>
2020-03-20 09:03:05 -0700

    axios call is working and props are set up

--- starwars/src/components/Characters.js
+++ starwars/src/components/Characters.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import CharactersList from './CharactersList'
 
 
-export default function Characters(){
+export default function Characters(props){
 
     return(
         <div className='characterCard'>
-        Hi!
+            <h1>Name:{props.name}</h1>
+        
       </div>
     )
 }
--- starwars/src/components/CharactersList.js
+++ starwars/src/components/CharactersList.js
@@ -1,3 +1,31 @@
 import React, {useState, useEffect} from 'react';
+import Characters from './Characters';
 import axios from 'axios';
 
+export default function CharactersList(){
+    const [data, setData]= useState([])
+
+    useEffect(() =>{
+        axios.get('https://swapi.co/api/people/')
+        .then(response => {
+            console.log('Axios Response:', response.data.results)
+            setData(response.data.results)
+        })
+        .catch(err => {
+            console.log('Axios Error:', err)
+        })
+    }, [])
+
+return(
+    <div className= 'CharacterCard'>
+        {data.map(item =>
+            <Characters name= {item.name}
+            key= {item.name}
+            height= {item.height}
+            weight= {item.mass}
+            hairColor= {item.hair_color}
+            skin= {item.skin_color}
+            />)}
+    </div>
+)
+}
